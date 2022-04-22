551a72054c220e946dd8bbc03c41efea4438c23a
毕克有 <596737739@qq.com>
2022-04-23 07:52:47 +0800

    feat: 理解下载文件流的形式, 几种返回方式, 生成xlsx文件

--- src/router/routes/upload/index.js
+++ src/router/routes/upload/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const Router = require('koa-router');
 const officegen = require('officegen');
+const xlsx = require('node-xlsx');
+const send = require('koa-send') // 将静态文件转换成文件流，提供下载功能
 
 
 let router = Router();
@@ -12,7 +14,6 @@ function getRandomNum(min, max) {
   return (max - min) * Math.random() + min;
 }
 
-// function getAllNum(min, max, count, sym, digits) {
 function getAllNum(query) {
   let { min, max, count, sym = '', digits, numRepeat = 1, symPosi = 1 } = query;
   let numArr = [];
@@ -27,20 +28,110 @@ function getAllNum(query) {
   return numArr.join('\n');
 }
 
+function getXlsxNum(query) {
+  let { min, max, count, sym = '', digits, numRepeat = 1, symPosi = 1 } = query;
+  let numArr = [];
+  const list = [
+    {
+      name: "sheet",
+      data: [
+        ["data1", "data2", "data3"],
+        ["data1", "data2", "data3"],
+        ["data1", "data4", "data3"],
+      ],
+    },
+  ];
+  for (let i = 0; i < count; i++) {
+    // console.log('getRandomNum(min, max)', getRandomNum(min, max));
+    let num = getRandomNum(min, max).toFixed(+digits);
+    if (numRepeat == 1 || (numRepeat == 2 && !numArr.includes(num))) {
+      numArr.push(symPosi == 1 ? [num + sym] : [sym + num])
+    }
+  }
+  list[0].data = numArr;
+  return list;
+}
+let i = 1;
+function writeStream () {
+  var data = '';
+  return new Promise((resolve, reject) => {
+    var readerStream = fs.createReadStream('./testFile.xlsx');
+    // 设置编码为 utf8。
+    readerStream.setEncoding('UTF8');
+
+    //要读取一个可读流中的数据，必须要给该流绑定一个data事件
+    //绑定data事件后，流会自动读取数据，数据读取完毕以后，流会自动关闭
+    //读取到的数据会通过回调函数的参数的形式返回
+
+    // 处理流事件 --> data, end, and error
+    readerStream.on('data', function(chunk) {
+      console.log('1111', i++)
+      data += chunk;
+    });
+
+    readerStream.on('end',function(){
+      console.log('----', data);
+      // resolve(readerStream);
+      resolve(data);
+    });
+
+    readerStream.on('error', function(err){
+      console.log(err.stack);
+    });
+  })
+}
+
 router.get('/getRandomNum', async (ctx, next) => {
-  // console.log('ctx.request.query', ctx.request.query);
-  // let {min, max, count, sym = '', digits, numRepeat=1, symPosi=1} = ctx.request.query;
-  let filename = 'randomNum';
-  ctx.set('Content-Disposition', 'attachment;filename=' + filename + '.txt');//attachment
-  ctx.set('Content-Type', 'application/txt');
-  // fs.writeFileSync( './randomNum.txt', '1.111 \n 1.222', { encoding: 'utf8' });
-  fs.writeFileSync('./randomNum.txt', getAllNum(ctx.request.query), { encoding: 'utf8' });
-  // let gReadData = fs.readFileSync('./randomNum.txt', { encoding: 'utf8' })
-  let gReadData = Buffer.from(fs.readFileSync('./randomNum.txt', 'utf-8'))
-  // ctx.success(
-  //   gReadData
-  // );
-  ctx.body = gReadData;
+  // // console.log('ctx.request.query', ctx.request.query);
+  // // let {min, max, count, sym = '', digits, numRepeat=1, symPosi=1} = ctx.request.query;
+  // let filename = 'randomNum';
+  // ctx.set('Content-Disposition', 'attachment;filename=' + filename + '.txt');//attachment
+  // ctx.set('Content-Type', 'application/txt');
+  // fs.writeFileSync('./randomNum.txt', getAllNum(ctx.request.query), { encoding: 'utf8' });
+  // let gReadData = Buffer.from(fs.readFileSync('./randomNum.txt', 'utf-8'))
+  // ctx.body = gReadData;
+
+  ctx.set('Content-Disposition', 'attachment;filename=testFile.xlsx');//attachment
+  ctx.set('Content-Type', 'application/octet-stream');
+  ctx.set('Access-Control-Expose-Headers', 'Content-Disposition');
+
+  const list = [
+    {
+      name: "sheet",
+      data: [
+        ["data1", "data2", "data3"],
+        ["data1", "data2", "data3"],
+        ["data1", "data4", "data3"],
+      ],
+    },
+  ];
+  
+  // 返回流 第一种方式
+  const buffer = xlsx.build(getXlsxNum(ctx.request.query));
+  ctx.body = buffer;
+
+
+  // 返回流 第二种方式
+  // const buffer = xlsx.build(getXlsxNum(ctx.request.query));
+  // fs.writeFileSync("./testFile.xlsx", buffer,  { encoding: 'utf8' });
+  // var readerStream = fs.createReadStream('./testFile.xlsx');
+  // // console.log('readerStream', readerStream);
+  // // await writeStream()
+  // ctx.body = readerStream;
+
+  // let gReadData = (fs.readFileSync('./testFile.xlsx', 'utf-8'))
+  // ctx.body = await writeStream();
+
+  // 这是 二进制 的， 需要返回流
+  // let gReadData = Buffer.from(fs.readFileSync('./testFile.xlsx', 'utf-8'))
+  // ctx.body = gReadData;
+  
+  // 第三种方式， 使用插件了
+  // const path = './testFile.xlsx';
+  // ctx.attachment(path)
+  // await send(ctx, path)
+
+
 })
 
 router.get('/getPDFRandomNum', async (ctx, next) => {
