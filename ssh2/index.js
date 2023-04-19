02ec6511dbb07f3610eee4f6dd45f3244f185edb
毕克有 <596737739@qq.com>
2023-04-19 09:56:21 +0800

    feat: ssh2连接服务器, 检查jenkins,停掉了就重新启动

---
+++ ssh2/index.js
@@ -0,0 +1,53 @@
+//...
+const { Client } = require('ssh2');
+const config = require('./config.js');
+const conn = new Client();
+// console.log('conn', conn);
+
+/**
+ * config 
+ * 
+ * host: '',
+   port: ,
+   username: '',
+   password: ''
+ */
+
+/**
+ * 
+ * @param {*} cmd 
+ * @returns 
+ */
+const myexec = (cmd) => {
+  return new Promise(resolve => {
+    conn.exec(cmd, (err, stream) => {
+      if (err) throw err;
+      let str = ''
+      stream.on('close', (code, signal) => {
+        console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+        // conn.end();
+        resolve(str);
+      }).on('data', (data) => {
+        //监听数据
+        str += data;
+      }).stderr.on('data', (data) => {
+        console.log('STDERR: ' + data);
+      });
+    });
+  })
+}
+
+conn.on('ready', async () => {
+  console.log('Client :: ready');
+  const res = await myexec('docker ps');
+  // console.log(res);
+  if (res.indexOf('426fae94ea33') !== -1) {
+    console.log('jenkins: 我还在运行');
+    conn.end();
+    return
+  }
+  console.log('jenkins: 我挂了，正在尝试启动');
+  myexec('docker start 426fae94ea33');
+
+}).connect(config);
+
---
+++ ssh2/test.js
@@ -0,0 +1,3 @@
+setInterval(() => {
+  console.log(1111);
+}, 1000);
